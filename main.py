1642ece8ae200c39606a486e78d32448c0bb68ff
Andrew D'Silva <ad21125@students.sacredheart.school.nz>
2025-08-18 21:20:27 +0000

    save

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from bottle import route, run, template, static_file
 
+@route('/static/<filepath:path>')
+def server_statc(filepath):
+    return static_file(filepath, root='./static')
+
+@route('/')
+def home():
+    return template('homepage')
+
+run(host='localhost', reloader=True)
